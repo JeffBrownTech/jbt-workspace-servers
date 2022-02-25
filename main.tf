1cb50f7e7008944d3c42fc5c85643e408ea1788f
Jeff Brown Tech <jeff@jeffbrown.tech>
2022-02-25 09:12:14 -0800

    add rg, change naming

--- main.tf
+++ main.tf
@@ -11,18 +11,37 @@ data "tfe_outputs" "ws-mgmt" {
   workspace    = "jbt-workspace-mgmt"
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-${local.prefix}}-servers"
+resource "azurerm_resource_group" "rg-identity" {
+  name     = "rg-${local.prefix}}-identity"
   location = "westus2"
 }
 
-resource "azurerm_network_interface" "nic" {
+resource "azurerm_resource_group" "rg-app1" {
+  name     = "rg-${local.prefix}}-app1"
+  location = "westus2"
+}
+
+resource "azurerm_network_interface" "identity01-nic" {
   name                = "identity01-nic"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg-identity.name
+  location            = azurerm_resource_group.rg-identity.location
   ip_configuration {
     name                          = "ipconfig"
     private_ip_address_allocation = "Dynamic"
     subnet_id                     = data.tfe_outputs.ws-mgmt.values.identity_snet_id
+  }
+}
+
+
+# To Do: create VM
+
+resource "azurerm_network_interface" "app1-01-nic" {
+  name                = "app1-01-nic"
+  resource_group_name = azurerm_resource_group.rg-app1.name
+  location            = azurerm_resource_group.rg-app1.location
+  ip_configuration {
+    name                          = "ipconfig"
+    private_ip_address_allocation = "Dynamic"
+    subnet_id                     = data.tfe_outputs.ws-mgmt.values.app1_snet_id
   }
 }
